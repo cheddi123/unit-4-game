046450edf5bcbbaaf1ebbb552d84db0fc81d115f
Cheddi Charles <cheddi.charles@gmail.com>
2018-09-08 23:30:16 -0500

    setting up html, js and css files

---
+++ assets/javascript/game.js
@@ -0,0 +1,78 @@
+
+
+var wins=0;
+var losses =0;
+var score=0;
+var ranNumber;
+var ifOverNumber =false;
+var buttonNumber;
+
+
+
+function initializer(){
+//    wins=0;
+//    losses=0;
+   score=0;
+
+   $("#score").text(score);
+//    $("#totalWins").text(wins);
+//    $("#totalLosses").text(losses);
+   ranNumber=Math.floor(Math.random()*110)+19;
+   console.log(ranNumber);
+
+   $("#computerRanNumber").text(ranNumber);
+  
+     buttonNumber=Math.floor(Math.random()*12)+1;
+   console.log(buttonNumber);  
+   $("#button1").val(buttonNumber)
+   
+   buttonNumber=Math.floor(Math.random()*12)+1;
+   console.log(buttonNumber);  
+   $("#button2").val(buttonNumber)
+
+   buttonNumber=Math.floor(Math.random()*12)+1;
+   console.log(buttonNumber);  
+   $("#button3").val(buttonNumber)
+
+   buttonNumber=Math.floor(Math.random()*12)+1;
+   console.log(buttonNumber);  
+   $("#button4").val(buttonNumber)
+
+}
+
+ 
+$(".button button").on("click",function(){
+    score += parseInt($(this).val()); 
+    
+         if(score<ranNumber){
+            
+            $("#score").text(score);
+            console.log(score);
+         }
+        
+        else if  (score === ranNumber) {
+            wins++;
+            $("#totalWins").text(wins);
+            alert("Congratulations, you reached the target number");
+            initializer();
+
+        }
+        
+      else{ 
+        losses++;
+        $("#totalLosses").text(losses);
+         
+        initializer();
+
+    }
+
+    
+    })
+
+  
+
+
+
+
+initializer();
+
